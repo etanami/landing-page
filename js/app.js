771dd4ee4c69763cf8d188e5e8f3de3689437849
unknown <etanamiolatunji@gmail.com>
2020-08-28 01:05:29 -0700

    added scroll to top button

--- js/app.js
+++ js/app.js
@@ -18,7 +18,9 @@
  * 
 */
 const allSections = document.querySelectorAll('section');
-const menuNav = document.getElementById('navbar__list');
+const menuNav = document.getElementById("navbar__list");
+const myBtn = document.getElementById("top_btn");
+
 
 /**
  * End Global Variables
@@ -74,12 +76,24 @@ document.querySelectorAll('a').forEach(a => {
     });
 });
 
+// scroll to top button when below fold of page
+window.onscroll = function() {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    	myBtn.style.display = "block";
+    }else{
+    	myBtn.style.display = "none";
+    }
+};
+
+function toTopFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 /**
  * End Main Functions
  * 
 */
-
-// Scroll to section on link click
 
 // Set sections as active
 
